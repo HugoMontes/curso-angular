afea05b04b2d6aed99195dc25bc1d29670fed1e7
Hugo Montes <hugomontes927@gmail.com>
2017-11-24 20:05:44 -0400

    Iniciando el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+// Importar el modulo Component del paquete @angular/core 
+// el cual fue descargado por npm 
 import { Component } from '@angular/core';
-
+// El anterior paquete nos provee un DECORADOR @Component
+// DECORADOR: Soporta una serie de metadatos y da funcionalidad
+// a la clase de abajo
 @Component({
+  // selector es una propiedad/metadato del decorador
+  // selector hace referencia a una etiqueta con el nombre app-root       
+  // donde se va cargar el componente, esta etiqueta esta en el 
+  // archivo curso-angular/src/index.html 
   selector: 'app-root',
+  // templateUrl: Asigna una VISTA/PLANTILLA a un archivo html el 
+  // cual permite imprimir informacion dentro de la clase del     
+  // componente
   templateUrl: './app.component.html',
+  // styleUrls: Definimos archivos css que afectan UNICAMENTE 
+  // al componente actual
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Cambiar el titulo por Hola Mundo
+  title = 'Hola Mundo';
 }
+
