dcd2a44995f14fc2aea05ef906bf2b673ff3e166
Hugo Montes <hugomontes927@gmail.com>
2017-11-29 18:06:38 -0400

    Se ha iniciado con el ejemplo de constructor

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   templateUrl: './persona.component.html',
 })
 export class PersonaComponent {
+  // Por convencion las propiedades se las inicializan en el constructor
   // Declarar las propiedades/atributos de la clase
   public titulo = 'Componente de Persona';
   // Declarar un atributo de tipo string
@@ -20,4 +21,20 @@ export class PersonaComponent {
   public profesiones:Array<string> = ['Carpintero','Pintor','Fontanero'];
   // Declarar un atributo de tipo array que contiene cualquier dato
   public miarray:Array<any> = ['Azul',12,true];
+
+  // Constructor: Se lo usan para inicializar las propiedades
+  // Se ejecuta una sola vez al instanciar un objeto
+  // Definir un constructor
+  constructor(){
+    // Inicializar propiedades
+    this.titulo='Componente de Persona';
+    this.nombre='Ana Gonzales';
+    this.edad=10;
+    this.mayorEdad=false;
+    this.comodin=100;
+    this.profesiones=['Carpintero','Pintor','Fontanero'];
+    this.miarray=['Azul',12,true];
+    // Mostrar el contenido de una propiedad
+    console.log(this.nombre);
+  }
 }
