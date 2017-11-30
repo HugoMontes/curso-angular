41f0257afcd9032e00eece9d814a86d80959726c
Hugo Montes <hugomontes927@gmail.com>
2017-11-30 12:19:06 -0400

    Se ha finalizado con los ejemplos de metodos y alcances de variables

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -22,8 +22,8 @@ export class PersonaComponent {
   // Declarar un atributo de tipo array que contiene cualquier dato
   public miarray:Array<any> = ['Azul',12,true];
 
-  // Constructor: Se lo usan para inicializar las propiedades
-  // Se ejecuta una sola vez al instanciar un objeto
+  // Constructor: Se lo usan para inicializar las propiedades y se
+  // ejecuta una sola vez al instanciar un objeto
   // Definir un constructor
   constructor(){
     // Inicializar propiedades
@@ -36,5 +36,42 @@ export class PersonaComponent {
     this.miarray=['Azul',12,true];
     // Mostrar el contenido de una propiedad
     console.log(this.nombre);
+    // Se puede ejecutar metodos dentro del constructor, aun que
+    // una buena practica es ejecutar las funciones en el metodo ngOnInit
+  }
+
+  // ngOnInit: Esta funcion se ejecuta inmediatamente despues de
+  // ejecutar el constructor y esta encargada de ejecutar cualquier funcion
+  // Ej: Llamar servicios, ejecutar funciones ajax
+  ngOnInit(){
+    // Llamar a una funcion
+    this.holaMundo(this.nombre);
+    // Llamar a una funcion con parametros
+    this.hola('Ariel');
+    // Llamar a una funcion que cambia valor de propiedad
+    this.cambiarEdad(55);
+    // Variables y alcances
+    var x=8;
+    var y=15;
+    if(x===8){
+      // Alcance de un let es unicamente dentro del bloque if
+      let x=3;
+      var y=88;
+      console.log('DENTRO DE IF: '+x+'-'+y);
+    }
+    console.log('FUERA DE IF: '+x+'-'+y);
+  }
+
+  // Definir una funcion
+  holaMundo(){
+    alert('Hola Mundo!');
+  }
+  // Definir una funcion con parametros
+  hola(nombre){
+    alert('Hola '+nombre);
+  }
+  // Definir una funcion que cambia valor de propiedad
+  cambiarEdad(edad){
+    this.edad=edad;
   }
 }
