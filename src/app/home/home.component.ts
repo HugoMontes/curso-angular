90c74d403d9280eff94032c87d5dad1a6f6a80ef
Hugo Montes <hugomontes927@gmail.com>
2017-12-13 13:00:19 -0400

    Se ha finalizado con el ejemplo de servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,20 +7,42 @@ import { ProductoService } from '../services/producto.service';
   templateUrl: './home.component.html',
   // Adicionar la propiedad providers que mediante la
   // inyeccion de depencias genera instancias de los servicios
-  providers: [ProductoService],
+  providers: [ProductoService]
 })
 export class HomeComponent{
 
   public titulo='Pagina Principal';
+  // Adicionar atributo listado de productos
+  public listado_productos:Array<string>;
+  // Adicionar atributo de producto a guardar
+  public producto_nuevo:string;
 
   // Crear una propiedad en el constructor para poder usar el servicio
   constructor(
     private _productoService: ProductoService
   ){}
-
+  // Funcion inicial
   ngOnInit(){
     // Llamar a las funciones de prueba del servicio
-    console.log(this._productoService.prueba());
-    console.log(this._productoService.pruebaSetNombre('Refrigerador'));
+    // console.log(this._productoService.prueba());
+    // console.log(this._productoService.pruebaSetNombre('Refrigerador'));
+    // Cargar el listado mediante el servicios
+    this.listado_productos=this._productoService.getProducto();
+    console.log(this.listado_productos);
+  }
+  // Definir metodo guardar producto
+  guardarProducto(){
+    // Adicionar nuevo producto mediante el servicio
+    this._productoService.addProducto(this.producto_nuevo);
+    // Limpiar campo de texto
+    this.producto_nuevo=null;
+  }
+  // Definir el metodo eliminar producto
+  eliminarProducto(index:number){
+    // alert(index);
+    if(confirm('Esta seguro de eliminar el producto?')){
+      // Eliminar producto mediante el servicio
+      this._productoService.deleteProducto(index);
+    }
   }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -2,16 +2,34 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ProductoService{
-
+  // Definir un atributo tipo string
   public nombre_producto='Mesa';
-
+  // Definir un atributyo tipo array
+  public coleccion_producto=['Televisor','Microondas'];
   // Crear el metodo prueba()
   prueba(){
     return this.nombre_producto;
   }
-
-  pruebaSetNombre(nombre_producto){
+  // Se define el metodo que recibe como argumento un nombre de producto y devuelve dicho nombre
+  // Es opcional indicar el tipo de argumentos y el tipo de retorno, simplemente puede ser:
+  // pruebaSetNombre(nombre_producto){...}
+  pruebaSetNombre(nombre_producto:string):string{
     this.nombre_producto=nombre_producto;
     return this.nombre_producto;
+  }
+  // Definir metodo para adicionar un elemento que devuelva el array de productos
+  addProducto(nombre_producto:string):Array<string>{
+    this.coleccion_producto.push(nombre_producto);
+    return this.getProducto();
+  }
+  // Definir metodo que retorna la coleccion
+  getProducto():Array<string>{
+    return this.coleccion_producto;
+  }
+  // Definir metodo para eliminar un producto de la coleccion
+  deleteProducto(index:number){
+    // Metodo splice(indice, nro_elem_a_borrar)
+    this.coleccion_producto.splice(index, 1);
+    return this.getProducto();
   }
 }
