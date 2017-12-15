b4976a822bf4d85c78fd23eec729ebfe127d8673
Hugo Montes <hugomontes927@gmail.com>
2017-12-15 13:32:02 -0400

    Se ha finalizado con el ejemplo de formulario

--- src/app/coche/coche.component.ts
+++ src/app/coche/coche.component.ts
@@ -8,13 +8,26 @@ import { Coche } from './coche';
 export class CocheComponent{
   // Definir una propiedad coche
   public coche:Coche;
+  // Definir un array de objetos coche
+  public coches:Array<Coche>;
+
   // Definir un constructor
   constructor(){
     this.coche = new Coche('',0,'');
+    // Inicializar el array de coches
+    this.coches=[
+      new Coche('Totoya',25000,'#000'),
+      new Coche('Subaru',15000,'#00f'),
+      new Coche('BMW',28000,'#f00')
+    ];
   }
   // Definir el metodo onSubmit
   onSubmit(){
     // Para probar mostrar los datos enviados
     console.log(this.coche);
+    // Adicionar el objeto a al colleccion
+    this.coches.push(this.coche);
+    // Limpiar el formulario
+    this.coche = new Coche('',0,'');
   }
 }
